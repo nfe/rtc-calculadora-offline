179e9d99d5ec262dd65159e0f40e1d14d5a30497
Gabriel Marquez <gabriel@nfe.io>
2025-09-24 11:11:05 -0300

    Add github release

--- code-backend/migrate.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ./calculadora/db/*
-
-mkdir -p ./calculadora/db
-
-./mvnw -Dflyway.configFiles=./flyway/flyway.conf flyway:migrate || true
--- code-backend/src/main/java/br/gov/serpro/rtc/config/xml/IMemoriaCalculo.java
+++
@@ -1,8 +0,0 @@
-package br.gov.serpro.rtc.config.xml;
-
-/**
- * Necessário para configurar a serialização da memória de cálculo no XML.
- */
-public interface IMemoriaCalculo {
-    String getMemoriaCalculo();
-}
